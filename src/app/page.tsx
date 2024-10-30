29b3abee3f7e38cdbd7c26b9f0d03703bced9885
Libo3 <valentinlibonati33@gmail.com>
2024-10-30 10:39:03 -0300

    feat creado HeaderHome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import HeaderHome from "@/components/others/HeaderHome";
 import QrSearcher from "@/components/others/QrSearcher";
 
 export default function Home() {
   return (
     <main className="max-w-screen-2xl mx-auto pt-8 px-4 sm:px-10 md:px-20 lg:px-28">
+      <HeaderHome />
       <QrSearcher />
     </main>
   );
