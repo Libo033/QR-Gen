cd24f082d9f52f993073074ae1e32180bb3e487a
Libo3 <valentinlibonati33@gmail.com>
2024-10-30 11:46:40 -0300

    styles cambios en componente QrSearcher

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ import QrSearcher from "@/components/others/QrSearcher";
 export default function Home() {
   return (
     <main className="max-w-screen-2xl mx-auto pt-8 px-4 sm:px-10 md:px-20 lg:px-28">
-      <LinkShortener />
+      <section className="flex flex-col gap-4">
+        <LinkShortener />
+        <QrSearcher />
+      </section>
     </main>
   );
 }
--- src/components/others/QrSearcher.tsx
+++ src/components/others/QrSearcher.tsx
@@ -2,22 +2,30 @@ import React from "react";
 
 const QrSearcher = () => {
   return (
-    <div className="w-full md:w-fit">
-      <div className="flex flex-col gap-1 w-full">
+    <div className="w-full pt-6 p-3 border rounded-lg shadow-lg md:w-fit">
+      <div className="text-3xl underline font-semibold text-center">
+        <p>Keep track</p>
+      </div>
+      <div className="pt-3">
+        <p className="text-sm text-center text-pretty">
+          If you already have an QR code, keep track of it.
+        </p>
+      </div>
+      <div className="flex flex-col gap-1 w-full pt-4">
         <label className="font-medium" htmlFor="qr-searcher">
           Search your QR
         </label>
-        <div className="flex gap-2">
-          <input
-            className="border px-2 py-3 rounded-lg outline-[var(--background)] w-full md:w-96"
-            id="qr-searcher"
-            placeholder="id"
-            type="text"
-          />
-          <button className="border border-[var(--background)] px-4 rounded-lg duration-150 hover:shadow-lg">
-            SEARCH
-          </button>
-        </div>
+        <input
+          className="border px-2 py-3 rounded-lg outline-[var(--background)] w-full md:w-96"
+          id="qr-searcher"
+          placeholder="id"
+          type="text"
+        />
+      </div>
+      <div className="flex flex-col gap-1 w-full pt-2">
+        <button className="border border-[var(--background)] px-4 py-3 rounded-lg duration-150 hover:shadow-md">
+          TRACE
+        </button>
       </div>
     </div>
   );
