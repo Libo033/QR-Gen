ff2873ba13ac5c602c2ee3ac29f269702f7d4f19
Libo3 <valentinlibonati33@gmail.com>
2024-10-30 10:45:33 -0300

    fix agregado boton a componente QrSearcher

--- src/components/others/QrSearcher.tsx
+++ src/components/others/QrSearcher.tsx
@@ -5,13 +5,19 @@ const QrSearcher = () => {
     <div className="w-full md:w-fit">
       <div className="flex flex-col gap-1 w-full">
         <label className="font-medium" htmlFor="qr-searcher">
-          QR Searcher
+          Search your QR
         </label>
-        <input
-          className="border px-2 py-3 rounded-lg outline-[var(--background)] w-full md:w-96"
-          id="qr-searcher"
-          type="text"
-        />
+        <div className="flex gap-2">
+          <input
+            className="border px-2 py-3 rounded-lg outline-[var(--background)] w-full md:w-96"
+            id="qr-searcher"
+            placeholder="id"
+            type="text"
+          />
+          <button className="border border-[var(--background)] px-4 rounded-lg duration-150 hover:shadow-lg">
+            SEARCH
+          </button>
+        </div>
       </div>
     </div>
   );
