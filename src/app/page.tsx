5eacd6938cb73020033c945d97f53ca2e1afa713
Libo3 <valentinlibonati33@gmail.com>
2024-10-30 11:10:30 -0300

    fix error con dependencias de react y react-dom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ export default function Home() {
   return (
     <main className="max-w-screen-2xl mx-auto pt-8 px-4 sm:px-10 md:px-20 lg:px-28">
       <HeaderHome />
-      <QrSearcher />
-      <LinkShortener />
     </main>
   );
 }
