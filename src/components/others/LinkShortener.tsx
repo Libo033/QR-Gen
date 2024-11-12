be277ae32ff54978b499a22484441c5c4e48dae2
Libo3 <valentinlibonati33@gmail.com>
2024-11-12 19:49:44 -0300

    feat en proceso

--- src/components/others/LinkShortener.tsx
+++ src/components/others/LinkShortener.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { useInView, animated } from "@react-spring/web";
-import React from "react";
+import React, { FormEvent, useState } from "react";
 
 const LinkShortener = () => {
+  const [created, setCreated] = useState<boolean>(false);
+  const [creationError, setCreationError] = useState<boolean>(true);
+
   const [ref, springs] = useInView(
     () => ({
       from: {
@@ -19,42 +22,60 @@ const LinkShortener = () => {
     }
   );
 
+  const handleShortURL = (Event: FormEvent) => {
+    try {
+      Event.preventDefault();
+
+      setCreated(true);
+    } catch (error) {
+      console.error("Error");
+    }
+  };
+
   return (
     <animated.div
       ref={ref}
       style={springs}
       className="w-full pt-6 p-3 border rounded-lg shadow-xl bg-white md:w-10/12 md:mx-auto lg:w-3/6 "
     >
-      <div className="text-3xl underline decoration-[#0099cc] font-semibold text-center">
-        <p>Short your URL</p>
-      </div>
-      <div className="flex flex-col gap-1 w-full pt-5">
-        <label className="font-medium" htmlFor="url-to-short">
-          URL
-        </label>
-        <input
-          className="border px-2 py-3 rounded-lg outline-[#ccf2ff] w-full"
-          id="url-to-short"
-          placeholder="Place your URL here"
-          type="text"
-        />
-      </div>
-      <div className="flex flex-col gap-1 w-full pt-2">
-        <label className="font-medium" htmlFor="url-alias">
-          Alias
-        </label>
-        <input
-          className="border px-2 py-3 rounded-lg outline-[#ccf2ff] w-full"
-          id="url-alias"
-          placeholder="Alias for your website"
-          type="text"
-        />
-      </div>
-      <div className="flex flex-col gap-1 w-full pt-2">
-        <button className="border border-[var(--background)] px-4 py-3 rounded-lg duration-150 hover:shadow-md">
-          CREATE URL
-        </button>
-      </div>
+      <section>
+        <form onSubmit={(Event: FormEvent) => handleShortURL(Event)}>
+          <div className="text-3xl underline decoration-[#0099cc] font-semibold text-center">
+            <p>Short your URL</p>
+          </div>
+          <div className="flex flex-col gap-1 w-full pt-5">
+            <label className="font-medium" htmlFor="url-to-short">
+              URL
+            </label>
+            <input
+              className="border px-2 py-3 rounded-lg outline-[#ccf2ff] w-full"
+              id="url-to-short"
+              placeholder="Place your URL here"
+              type="text"
+            />
+          </div>
+          <div className="flex flex-col gap-1 w-full pt-2">
+            <label className="font-medium" htmlFor="url-alias">
+              Alias
+            </label>
+            <input
+              className="border px-2 py-3 rounded-lg outline-[#ccf2ff] w-full"
+              id="url-alias"
+              placeholder="Alias for your website"
+              type="text"
+            />
+          </div>
+          <animated.div className="flex flex-col gap-1 w-full pt-2">
+            <button
+              type="submit"
+              className="border border-[var(--background)] px-4 py-3 rounded-lg duration-150 hover:shadow-md"
+            >
+              CREATE URL
+            </button>
+          </animated.div>
+        </form>
+      </section>
+      <section></section>
     </animated.div>
   );
 };
